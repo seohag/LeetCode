c1700d2bd6080e08ec52d60f7ccd7124c64a109e
seohag <126459089+seohag@users.noreply.github.com>
2024-03-30 22:04:32 +0900

    Time: 49 ms (85.95%), Space: 49.3 MB (64.08%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return "";
+    }
+
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 0; j < strs.length; j++) {
+            if (strs[0][i] !== strs[j][i]) {
+                return strs[0].slice(0, i);
+            }
+        }
+    }
+    
+    
+    
+    return strs[0];
+};
