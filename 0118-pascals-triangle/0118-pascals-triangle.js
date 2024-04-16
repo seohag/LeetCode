3d544a99819978ccbcbfa5dbf351ebd21293c751
seohag <126459089+seohag@users.noreply.github.com>
2024-04-16 13:50:15 +0900

    Time: 43 ms (98.77%), Space: 50.1 MB (51.90%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -7,10 +7,13 @@ const generate = function(numRows) {
     
     for (let i = 0; i < numRows; i++) {
         result[i] = [];
-        result[i][0] = result[i][i] = 1;
+        result[i][0] = 1;
+        
         for (let j = 1; j < i; j++) {
-            result[i][j] = result[i - 1][j] + result[i - 1][j - 1];
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
         }
+    
+        result[i][i] = 1;
     }
     
     return result;
