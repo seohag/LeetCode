5bcd8dd795b8bcba42c2ba47ef2973b04c4b3796
seohag <126459089+seohag@users.noreply.github.com>
2024-04-26 12:01:30 +0900

    Time: 50 ms (55.75%), Space: 48.8 MB (42.30%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+const convertToTitle = function(columnNumber) {
+    const result = [];
+    
+    while (columnNumber > 0) {
+        const numberToCharacter = columnNumber % 26;
+        
+        if (numberToCharacter === 0) {
+            result.push("Z");
+            columnNumber = Math.floor(columnNumber / 26) - 1;
+        } else {
+            result.push(String.fromCharCode((numberToCharacter - 1) + "A".charCodeAt(0)));
+            columnNumber = Math.floor(columnNumber / 26);
+        }
+    }
+    
+    
+    
+    return result.reverse().join("");
+};
