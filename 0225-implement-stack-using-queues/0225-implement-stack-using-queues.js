bd2a2a30b1b9e2219f47756cd332dc4c18c5a9c3
seohag <126459089+seohag@users.noreply.github.com>
2024-05-06 15:32:37 +0900

    Time: 50 ms (62.44%), Space: 48.9 MB (31.80%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -0,0 +1,55 @@
+
+const MyStack = function() {
+    this.mainQueue = [];
+    this.subQueue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+        while (this.mainQueue.length > 0) {
+            this.subQueue.push(this.mainQueue.shift());
+        }
+    
+        this.mainQueue.push(x);
+
+        while (this.subQueue.length > 0) {
+            this.mainQueue.push(this.subQueue.shift());
+        }
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.mainQueue.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.mainQueue[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    if (this.mainQueue.length === 0) {
+        return true;
+    }
+    
+    return false;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
