42cd91356c1a637183a773f4535ff6498499dad8
seohag <126459089+seohag@users.noreply.github.com>
2024-05-28 12:08:09 +0900

    Time: 69 ms (68.60%), Space: 52.3 MB (64.80%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfFour = function(n) {
+    while (n && n % 4 == 0) {
+	    n /= 4;
+    }
+    
+    if (n === 1) {
+        return true;
+    } else {
+        return false
+    }
+};
+
