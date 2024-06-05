d96780abde30a0ecd8db6a3332948aed66307f09
seohag <126459089+seohag@users.noreply.github.com>
2024-06-05 18:27:07 +0900

    Time: 58 ms (16.19%), Space: 48.6 MB (65.64%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const guessNumber = function(n) {
+    function findNumber(start, end) {
+        const mid = Math.floor((end + start) / 2);
+        
+        if (guess(mid) === 0) {
+            return mid;
+        }
+        
+        if (guess(mid) === -1) {
+            return findNumber(start, mid - 1);
+        }
+        
+        if (guess(mid) === 1) {
+            return findNumber(mid + 1, end);
+        }
+    }
+    
+    return findNumber(0, n);
+};
