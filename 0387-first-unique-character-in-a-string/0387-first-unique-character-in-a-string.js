0bfbeb72416a612e9e4b781bacaa020c964eb93f
seohag <126459089+seohag@users.noreply.github.com>
2024-06-10 15:15:55 +0900

    Time: 84 ms (77.24%), Space: 52 MB (96.11%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,22 +3,9 @@
  * @return {number}
  */
 const firstUniqChar = function(s) {
-    const result = {};
-    
     for (let i = 0; i < s.length; i++) {
-        const character = s[i];
-        
-        if (Object.keys(result).includes(character)) {
-            let count = result[character];
-            result[character] = count + 1;
-        } else {
-            result[character] = 1;
-        }
-    }
-    
-    for (character in result) {
-        if (result[character] === 1) {
-            return s.indexOf(character);
+        if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+            return i;
         }
     }
     
