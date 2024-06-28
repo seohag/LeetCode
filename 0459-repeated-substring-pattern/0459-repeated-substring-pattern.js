51054f98df0c75e6ba5fd73105aaf65c8465b608
seohag <126459089+seohag@users.noreply.github.com>
2024-06-28 17:56:20 +0900

    Time: 1047 ms (5.11%), Space: 56.6 MB (5.11%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -3,20 +3,15 @@
  * @return {boolean}
  */
 const repeatedSubstringPattern = function(s) {
-    for (let i = 1; i < s.length; i++) {
-        const pattern = s.slice(0, i);
-        let isRepeated = true;
-        
-        for (let j = i; j < s.length; j += pattern.length) {
-            const cmp = s.slice(j, j + pattern.length);
-            
-            if (pattern !== cmp) {
-                isRepeated = false;
-                break;
-            }
-        }
-        
-        if (isRepeated) {
+    if (s.length < 2) {
+        return false;
+    }
+    
+    let pattern = "";
+    
+    for (let i = 0; i < s.length / 2; i++) {
+        pattern += s[i];
+        if (!s.replaceAll(pattern, "")) {
             return true;
         }
     }
