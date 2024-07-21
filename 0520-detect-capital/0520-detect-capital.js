f12b992e2ef25b61cffb4b862c184073886ad8f1
seohag <126459089+seohag@users.noreply.github.com>
2024-07-21 22:12:36 +0900

    Time: 58 ms (51.79%), Space: 48.8 MB (85.52%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+const detectCapitalUse = function(word) {
+    if (word === word.toUpperCase()) {
+        return true;
+    }
+    
+    if (word === word.toLowerCase()) {
+        return true;
+    }
+    
+    if (word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()) {
+        return true;
+    }
+    
+    return false;
+};
