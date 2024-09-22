728ad5d929cca510fe9605b6a048a34c55359081
seohag <126459089+seohag@users.noreply.github.com>
2024-09-22 21:31:19 +0900

    Time: 76 ms (58.33%), Space: 55.5 MB (57.66%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for node.
+ * class _Node {
+ *     val: number
+ *     children: _Node[]
+ *     constructor(val?: number) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = []
+ *     }
+ * }
+ */
+
+function postorder(root: _Node | null): number[] {
+  const result: number[] = [];
+
+    const helperFunction = (node: Node | null) => {
+        if (!node) return;
+        
+        for (const child of node.children) {
+            helperFunction(child);
+        }
+        
+        result.push(node.val);
+    };
+
+    helperFunction(root);
+
+    return result;
+};
