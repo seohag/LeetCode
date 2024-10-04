33071ca201e4efb2781ee8daa44e27e020293448
seohag <126459089+seohag@users.noreply.github.com>
2024-10-04 18:10:59 +0900

    Time: 64 ms (59.21%), Space: 51.7 MB (78.95%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.ts
@@ -0,0 +1,14 @@
+function isToeplitzMatrix(matrix: number[][]): boolean {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    
+    for (let i = 0; i < rows - 1; i++) {
+        for (let j = 0; j < cols - 1; j++) {
+            if (matrix[i][j] !== matrix[i + 1][j + 1]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+};
