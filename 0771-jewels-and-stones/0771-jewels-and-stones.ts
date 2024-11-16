6e633c47a2006ae57e60ecb8c7f3b09a2898d12c
seohag <126459089+seohag@users.noreply.github.com>
2024-11-16 17:04:50 +0900

    Time: 0 ms (100.00%), Space: 51.7 MB (61.69%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.ts
+++ 0771-jewels-and-stones/0771-jewels-and-stones.ts
@@ -1,12 +1,11 @@
 function numJewelsInStones(jewels: string, stones: string): number {
     let result = 0;
-    const jewelSet = new Set(jewels);
 
     for (const stone of stones) {
-        if (jewelSet.has(stone)) {
-            result += 1;
+        if (jewels.includes(stone)) {
+            result++;
         }
     }
-    
+
     return result;
 };
